648f9f40e4616909a4304d22dcf5f90db584fb67
mkpage1337 <mrku1337@gmail.com>
2025-01-13 19:14:55 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+import { ServerStatusComponent } from "./bashboard/server-status/server-status.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [HeaderComponent, ServerStatusComponent],
 })
 export class AppComponent {
   dummyTrafficData = [
@@ -37,5 +40,4 @@ export class AppComponent {
     },
   ];
   maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
-  currentStatus = 'online';
 }
