7cbed5f1b17390e481bf732f5f44d67e8f8cef67
mkpage1337 <mrku1337@gmail.com>
2025-01-13 19:31:34 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './bashboard/server-status/server-status.component';
-import { TrafficComponent } from "./bashboard/traffic/traffic.component";
+import { TrafficComponent } from './bashboard/traffic/traffic.component';
+import { TicketsComponent } from './bashboard/tickets/tickets.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent],
+  imports: [
+    HeaderComponent,
+    ServerStatusComponent,
+    TrafficComponent,
+    TicketsComponent,
+  ],
 })
 export class AppComponent {}
---
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bashboard-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './bashboard-item.component.html',
+  styleUrl: './bashboard-item.component.css'
+})
+export class DashboardItemComponent {
+
+}
