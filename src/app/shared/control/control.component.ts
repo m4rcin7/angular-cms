fba5ed6b4d6e1f1e6dba42db2b1f405a4edcc6b8
mkpage1337 <mrku1337@gmail.com>
2025-01-16 21:48:41 +0100

    commit

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
 })
 export class ControlComponent {
-
+  label = input.required<string>();
 }
