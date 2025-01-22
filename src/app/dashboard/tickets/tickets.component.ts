48a9e955ffc69737dcd599e2206f1003d2ad981d
mkpage1337 <mrku1337@gmail.com>
2025-01-22 14:42:53 +0100

    commit

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { Ticket } from './ticket.model';
+import { NewTicketComponent } from './new-ticket/new-ticket.component';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [],
+  imports: [NewTicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
 })
 export class TicketsComponent {
   tickets: Ticket[] = [];
+
+  onAdd(ticketData: { title: string; text: string }) {
+    const ticket: Ticket = {
+      title: ticketData.title,
+      request: ticketData.text,
+      id: Math.random().toString(),
+      status: 'open',
+    };
+
+    this.tickets.push(ticket);
+  }
 }
